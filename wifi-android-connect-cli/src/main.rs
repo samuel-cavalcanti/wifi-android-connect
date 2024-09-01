0eb65582c23afffc3fb153a0ea62e69dcc3c812f
samuel-cavalcanti <scavalcanti111@gmail.com>
2024-09-01 13:16:26 -0300

    refactor: move code to lib

--- wifi-android-connect-cli/src/main.rs
+++ wifi-android-connect-cli/src/main.rs
@@ -1,3 +1,10 @@
+use wifi_android_connect_lib::WifiAndroidConnect;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+    let con = WifiAndroidConnect::default();
+
+    println!("{}", con.qrcode_img());
+
+    con.connect().unwrap()
 }
--- wifi-android-connect-lib/src/lib.rs
+++ wifi-android-connect-lib/src/lib.rs
@@ -1,5 +1,101 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod adb_device_authentication;
+mod adb_zero_conf;
+mod client;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use adb_device_authentication::{AdbDeviceAuthentication, AdbService};
+use adb_zero_conf::AdbZeroConf;
+use client::RustAdbClient;
+use qrcode::{render::unicode, QrCode};
+use rand::Rng;
+
+fn wifi_connect_msg(name: &str, pair_code: u32) -> String {
+    assert!(
+        (100_000..999_999).contains(&pair_code),
+        "Should be a 6 digits number"
+    );
+    format!(
+        "WIFI:T:ADB;S:{hostname};P:{password};;",
+        hostname = name,
+        password = pair_code
+    )
+}
+
+fn generate_qrcode_img(data: String) -> String {
+    let code = QrCode::new(data).unwrap();
+    code.render::<unicode::Dense1x2>()
+        .dark_color(unicode::Dense1x2::Light)
+        .light_color(unicode::Dense1x2::Dark)
+        .build()
+}
+
+fn random_6_digits_pair_code() -> u32 {
+    rand::thread_rng().gen_range(100_000..999_999)
+}
+
+fn on_pair(auth: Rc<RefCell<AdbDeviceAuthentication>>) -> impl Fn(AdbService) {
+    move |s| {
+        auth.borrow_mut().on_pair(&s, &RustAdbClient);
+        log::trace!("Auth state: {auth:?}");
+        log::trace!("service: {s:?}");
+    }
+}
+fn on_connect(auth: Rc<RefCell<AdbDeviceAuthentication>>) -> impl Fn(AdbService) {
+    move |s| {
+        auth.borrow_mut().on_connect(&s, &RustAdbClient);
+        log::trace!("Auth state: {auth:?}");
+        log::trace!("service: {s:?}");
+    }
+}
+
+pub struct WifiAndroidConnect {
+    pub pair_name: String,
+    pub pair_code: u32,
+}
+
+impl Default for WifiAndroidConnect {
+    fn default() -> Self {
+        Self {
+            pair_name: "WIFI Android Connect".into(),
+            pair_code: random_6_digits_pair_code(),
+        }
+    }
+}
+
+impl WifiAndroidConnect {
+    pub fn new(pair_name: String, pair_code: u32) -> Self {
+        WifiAndroidConnect {
+            pair_name,
+            pair_code,
+        }
+    }
+    pub fn qrcode_img(&self) -> String {
+        let code = wifi_connect_msg(&self.pair_name, self.pair_code);
+        generate_qrcode_img(code)
+    }
+    pub fn connect(&self) -> Result<(), String> {
+        let auth = AdbDeviceAuthentication::new(self.pair_code, self.pair_name.to_string());
+        let auth = RefCell::new(auth);
+        let auth = Rc::new(auth);
+
+        let zeroconf = AdbZeroConf::new(
+            Box::new(on_pair(auth.clone())),
+            Box::new(on_connect(auth.clone())),
+        );
+
+        loop {
+            if let Err(e) = zeroconf.poll() {
+                log::error!("Poll error: {e:?}");
+                return Err(e.to_string());
+            }
+
+            if auth.borrow_mut().is_connected() {
+                return Ok(());
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +103,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[should_panic]
+    fn test_wifi_msg_1_digit() {
+        let _msg = wifi_connect_msg("connectAndroid", 5);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_wifi_msg_5_digits() {
+        let _msg = wifi_connect_msg("connectAndroid", 12345);
+    }
+
+    #[test]
+    fn test_wifi_msg_6_digit() {
+        let msg = wifi_connect_msg("connectAndroid", 765912);
+        assert_eq!(msg, "WIFI:T:ADB;S:connectAndroid;P:765912;;");
+        let msg = wifi_connect_msg("connect Android", 123456);
+        assert_eq!(msg, "WIFI:T:ADB;S:connect Android;P:123456;;");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_wifi_msg_7_digits() {
+        let _msg = wifi_connect_msg("connectAndroid", 1234567);
     }
 }
