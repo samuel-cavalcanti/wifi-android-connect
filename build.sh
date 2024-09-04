58a483552be482233514e4bf2b7d7874cb7bdf58
samuel-cavalcanti <scavalcanti111@gmail.com>
2024-09-04 19:54:09 -0300

    feat: make this repo the plugin repo

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cargo b --release -p=wifi-android-connect-nvim 
-cp ./target/release/libwifi_android_connect_nvim.so ./wifi-android-connect-nvim/lua/
+cp ./target/release/libwifi_android_connect_nvim.so ./lua/
